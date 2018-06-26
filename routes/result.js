bc8238e3015b8057dafd8ceff2f9be1e1d4c7d73
Athiwat <athivvat@gmail.com>
2018-06-26 17:05:23 +0700

    Validate

--- routes/result.js
+++ routes/result.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router(); 
 var mongoose = require('mongoose');
 var User = mongoose.model('users');
+var _ = require('lodash')
 
 router.post('/', function(req, res, next) {
-  console.log(req.body.employeeId)
-  User.find(function(err, users){
-    console.log(users)
-    res.render('result', { title: 'โปรแกรมคุกกี้เสียงทาย' });
+  var employeeId = req.body.employeeId
+  User.find({employeeId}, function(err, users){
+    if (_.isEmpty(users)) {
+      var message = 'ไม่พบข้อมูลรหัสพนักงาน ' + employeeId + ' กรุณาตรวจสอบและเสี่ยงทายอีกครั้ง'
+      res.render('result', { 
+        title: 'โปรแกรมคุกกี้เสียงทาย',
+        message
+      });
+    } else {
+      res.render('result', { title: 'โปรแกรมคุกกี้เสียงทาย' });
+    }
   });
 });
 
