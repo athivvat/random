ed66aa17f36b1635f419ca307b5b457486f93cf2
Athiwat <athivvat@gmail.com>
2018-07-02 11:25:19 +0700

    Show Result

--- routes/match.js
+++ routes/match.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router(); 
 var mongoose = require('mongoose');
 var User = mongoose.model('users');
+var _ = require('lodash')
 
 router.get('/', function(req, res, next) {
-    res.render('match', { title: 'โปรแกรมคุกกี้เสียงทาย' });
+  User.find({}, function(err, users) {
+    console.log(users)
+    res.render('match', { 
+      title: 'โปรแกรมคุกกี้เสียงทาย',
+      users
+    });
+  })
   });
   
   module.exports = router;
